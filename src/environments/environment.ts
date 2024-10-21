3a8235058f1845a2731a6b8a1e824d4516571863
thiagofrede <thiagofredes1@gmail.com>
2024-10-21 10:45:11 -0300

    environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,16 @@
 export const enviremont = {
-    production: false
-}
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyAaOdmab7G3kaOUfTSOTJZAi50RpH6zbU8",
+        authDomain: "programacion1-2024.firebaseapp.com",
+        projectId: "programacion1-2024",
+        storageBucket: "programacion1-2024.appspot.com",
+        messagingSenderId: "725933232758",
+        appId: "1:725933232758:web:c3ab05635c1950d0624557"
+      }
+    }
+      
+      
+      
+      
+      
