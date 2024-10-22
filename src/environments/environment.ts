51b9c6ade84035f74bacdcbf98095de3ed9c2a42
tristelme <francojulieta78@gmail.com>
2024-10-21 21:03:31 -0300

    firebaselisto

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,12 @@
 export const enviremont = {
-    production: false
+    production: false,
+    // Your web app's Firebase configuration
+ firebaseConfig: {
+    apiKey: "AIzaSyBltuKAfDGPm6CbVk48CtxlyKhbQ_WzQAg",
+    authDomain: "phone2-a8947.firebaseapp.com",
+    projectId: "phone2-a8947",
+    storageBucket: "phone2-a8947.appspot.com",
+    messagingSenderId: "561638922919",
+    appId: "1:561638922919:web:817db7b6f364c36e145982"
+  }
 }
